b97500df45f4054dc4b70f42cadc5560a3e06e3d
Ngoc-Danh <danhtruong0704@gmail.com>
2026-05-17 11:00:36 +0700

    Refactor BookingService for clarity and price calculation

--- cinema_backend/src/main/java/com/cinema/project/service/BookingService.java
+++ cinema_backend/src/main/java/com/cinema/project/service/BookingService.java
@@ -31,12 +31,12 @@ public class BookingService {
 
         List<Ticket> savedTickets = new ArrayList<>();
 
-        // 1. Lấy danh sách các ghế ĐÃ BÁN của riêng suất chiếu này để kiểm tra
+        //  Lấy danh sách các ghế ĐÃ BÁN của riêng suất chiếu này để kiểm tra
         List<String> bookedSeatIds = ticketRepository.findBookedSeatIdsByShowtime(request.getShowtimeId());
 
         for (String seatId : request.getSeatIds()) {
 
-            // 2. Chặn ngay nếu phát hiện ghế đã có người đặt trong suất chiếu này
+            //  Chặn ngay nếu phát hiện ghế đã có người đặt trong suất chiếu này
             if (bookedSeatIds.contains(seatId)) {
                 throw new RuntimeException("Rất tiếc, một trong các ghế bạn chọn đã có người đặt!");
             }
@@ -44,17 +44,15 @@ public class BookingService {
             Seat seat = seatRepository.findById(seatId)
                     .orElseThrow(() -> new RuntimeException("Không tìm thấy Ghế"));
 
-            // KHÔNG CÒN GỌI seat.setIsOccupied() Ở ĐÂY NỮA
-
-            // 3. Backend tự tính giá an toàn dựa trên loại ghế
-            double ticketPrice = 30000; // Mặc định ghế THUONG
+            //  Backend tự tính giá an toàn dựa trên loại ghế
+            double ticketPrice = 30000; // Mặc định lấy giá ghế THƯỜNG
             if ("VIP".equalsIgnoreCase(seat.getSeatType())) {
                 ticketPrice = 50000;
             } else if ("DOI".equalsIgnoreCase(seat.getSeatType())) {
                 ticketPrice = 100000;
             }
 
-            // 4. Tạo vé mới
+        
             Ticket ticket = new Ticket();
             String randomTicketId = "TK-" + UUID.randomUUID().toString().substring(0, 5).toUpperCase();
 
@@ -62,7 +60,7 @@ public class BookingService {
             ticket.setUser(user);
             ticket.setShowtime(showtime);
             ticket.setSeat(seat);
-            ticket.setTotalPrice(ticketPrice); // Giá thực tế của từng vé
+            ticket.setTotalPrice(ticketPrice); 
             ticket.setStatus("SUCCESS");
             ticket.setBookingDate(LocalDateTime.now());
 
