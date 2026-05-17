e02948ea6a50226d0ff27c29d34e9892da104ecf
HoangQuan_Gon <quanbui0208@gmail.com>
2026-05-17 10:37:22 +0700

    update ve
    Removed commented code explaining the purpose of the query.

--- cinema_backend/src/main/java/com/cinema/project/repositories/TicketRepository.java
+++ cinema_backend/src/main/java/com/cinema/project/repositories/TicketRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, String> {
-    // Tìm danh sách ID ghế đã có người mua thành công trong 1 suất chiếu cụ thể
     @Query("SELECT t.seat.seatId FROM Ticket t WHERE t.showtime.showtimeId = :showtimeId AND t.status = 'SUCCESS'")
     List<String> findBookedSeatIdsByShowtime(@Param("showtimeId") String showtimeId);
 }
