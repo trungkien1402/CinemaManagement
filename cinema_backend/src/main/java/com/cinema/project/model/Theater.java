422fa332defc3a73af0d13350724b2eb1e99f420
Ngoc-Danh <danhtruong0704@gmail.com>
2026-05-17 10:31:23 +0700

    update theather
    Updated column definition for 'location' to nvarchar for Vietnamese support.

--- cinema_backend/src/main/java/com/cinema/project/model/Theater.java
+++ cinema_backend/src/main/java/com/cinema/project/model/Theater.java
@@ -21,7 +21,6 @@ public class Theater {
     @Column(name = "city", length = 50, columnDefinition = "nvarchar(50)")
     private String city;
 
-    // 💡 Đã gộp và sửa lại thành nvarchar để nhận tiếng Việt có dấu từ DB
     @Column(name = "location", length = 255, columnDefinition = "nvarchar(255)")
     private String location;
 
