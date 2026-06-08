8abc744e17a093fb9e251821dec925cfef25cab0
minhtrien29 <minhtrien3103@gmail.com>
2026-06-09 05:12:33 +0700

    Fix: add global response interceptor to default axios instance to rewrite localhost:8080 URLs in production

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,30 @@ import store from './store/store.js'
 import './i18n.js';
 import axios from 'axios';
 
+const replaceLocalhost = (obj) => {
+  if (typeof obj === 'string') {
+    if (obj.startsWith('http://localhost:8080')) {
+      if (!window.location.hostname.includes('localhost') && window.location.hostname !== '127.0.0.1') {
+        return obj.replace('http://localhost:8080', window.location.origin);
+      }
+    }
+    return obj;
+  }
+  if (Array.isArray(obj)) {
+    return obj.map(replaceLocalhost);
+  }
+  if (obj !== null && typeof obj === 'object') {
+    const newObj = {};
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        newObj[key] = replaceLocalhost(obj[key]);
+      }
+    }
+    return newObj;
+  }
+  return obj;
+};
+
 // Configure global axios interceptor to replace localhost:8080 dynamically in production
 axios.interceptors.request.use((config) => {
   if (config.url && config.url.startsWith('http://localhost:8080')) {
@@ -18,6 +42,14 @@ axios.interceptors.request.use((config) => {
 }, (error) => {
   return Promise.reject(error);
 });
+
+axios.interceptors.response.use((response) => {
+  response.data = replaceLocalhost(response.data);
+  return response;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 
 
 createRoot(document.getElementById('root')).render(
